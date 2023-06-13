5cdb1e597f1b398c6b801c74870909efca4ac640
iisk145 <iisk145@gmail.com>
2023-06-13 15:22:42 +0900

    자바1번

---
+++ ch01/src/ch01/Hello.java
@@ -0,0 +1,26 @@
+package ch01;
+
+/**
+ * 날짜 : 2023 / 06 / 12 
+ * 이름 : 이규석 
+ * 내용 : ch01.Java 개발환경 구축
+ * 
+ * @author issi0
+ *
+ */
+public class Hello {
+//  진입점
+	public static void main(String[] args) {
+// 기본출력
+		System.out.println("Hello World");
+		System.out.println("Hello Java");
+
+		//한 줄 출력
+		System.out.print("Hello ");
+		System.out.print("Korea");
+		
+		// 개행처리 (줄바꿈)( \n )
+		System.out.print("\n");
+		System.out.print("Hello Busan");
+	}
+}
