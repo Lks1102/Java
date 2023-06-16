6e8cd9149da0e4e8f69949967f465789ed3f2505
iisk145 <iisk145@gmail.com>
2023-06-16 17:25:38 +0900

    코테

---
+++ CodingTest/src/sub01/test3.java
@@ -0,0 +1,21 @@
+package sub01;
+
+import java.util.Scanner;
+
+public class test3 {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+		System.out.println();
+		
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+
+	}
+}
