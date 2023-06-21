bc9d2bee802a815e16e469714c9bf478f5a4b29f
iisk145 <iisk145@gmail.com>
2023-06-21 10:35:03 +0900

    예제

--- Exam/src/re/BackJoon.java
+++ Exam/src/re/BackJoon.java
@@ -2,22 +2,17 @@ package re;
 
 import java.util.Scanner;
 
-public class BackJoon {
-	
-	public static void main(String[] args) {
-	
+public class BackJoon {	
+	public static void main(String[] args) {		
 		Scanner sc = new Scanner(System.in);
-		
-		int num1;
-		int num2;
-		
-		num1 = sc.nextInt();		
-		num2 = sc.nextInt();
-		int a = num1 * (num2 % 10);
-		int b = num1 * ((num2/10) % 10);
-		int c = num1 * (num2 / 100);
-		int result = num1 * num2;
-		
+				
+		int a = sc.nextInt();		
+		int b = sc.nextInt();
+				
+		System.out.println(a * (b % 10));
+		System.out.println(a * ((b / 10) % 10));
+		System.out.println(a * (b / 100));
+		System.out.println(a * b);
 		sc.close();
 	}
 }
