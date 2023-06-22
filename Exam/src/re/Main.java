1e468f48248c374849d74fd4cf3ff8cf80aa5c65
iisk145 <iisk145@gmail.com>
2023-06-22 10:46:53 +0900

    예제

--- Exam/src/re/Main.java
+++ Exam/src/re/Main.java
@@ -3,17 +3,17 @@ package re;
 import java.util.Scanner;
 
 class Main {
+/*
+ * 
+ */
+ public static void main(String[] args) {
+	 
+	Scanner sc = new Scanner(System.in);
+	
 
-	public static void main(String[] args) {
 
-		Scanner sc = new Scanner(System.in);
-		int a = sc.nextInt();
-		sc.close();
+
 		
-		if() {
-			System.out.println("1");
-		}else if() {
-			System.out.println("0");
-		}
 	}
+ 	
 }
