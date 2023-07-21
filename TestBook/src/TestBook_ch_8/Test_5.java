d84d1ed796f09ee460d57d228f7119b005ae38d6
iisk145 <iisk145@gmail.com>
2023-07-21 16:30:14 +0900

    test_8

--- TestBook/src/TestBook_ch_8/Test_5.java
+++ TestBook/src/TestBook_ch_8/Test_5.java
@@ -1,5 +1,23 @@
 package TestBook_ch_8;
 
-public class Test_5 {
+interface Remocon {
+	public void powerOn();
+}
 
+class TV implements Remocon{
+
+	@Override
+	public void powerOn() {
+		System.out.println("Tv를 켯습니다.");
+	}
+	
+}
+
+public class Test_5 {
+	public static void main(String[] args) {
+		
+		Remocon r = new TV();
+		r.powerOn();
+		
+	}
 }
